c7ea8bb81b93f6ead8029f2653b2d37b46f03e1a
mellic03 <mellic03@gmail.com>
2026-08-04 05:31:17 +1000

    Automated commit

--- src/GfxService.cpp
+++ src/GfxService.cpp
@@ -1,54 +1,54 @@
-#include "idk/GfxService.hpp"
-#include "libidk/New.hpp"
+// #include "idk/GfxService.hpp"
+// #include "libidk/New.hpp"
 
 
-idk::GfxService::GfxService(idk::Platform &plat)
-:   IDK_SERVICE_CTOR(GfxService),
-    mPlat(plat),
-    mRen(plat)
-{
-    uint64_t tickRateHz = mCfg["TICKRATE_HZ"].toU64();
-    mRen.setRefreshRateHz(tickRateHz);
-    VLOG_INFO("[GfxService::GfxService] tickRateHz={}", tickRateHz);
-}
+// idk::GfxService::GfxService(idk::Platform &plat)
+// :   IDK_SERVICE_CTOR(GfxService),
+//     mPlat(plat),
+//     mRen(plat)
+// {
+//     uint64_t tickRateHz = mCfg["TICKRATE_HZ"].toU64();
+//     mRen.setRefreshRateHz(tickRateHz);
+//     VLOG_INFO("[GfxService::GfxService] tickRateHz={}", tickRateHz);
+// }
 
 
-idk::GfxService::~GfxService()
-{
+// idk::GfxService::~GfxService()
+// {
 
-}
-
-
-void idk::GfxService::update(idk::IEngine *E)
-{
-    mPlat.update();
-    mRen.update(E);
-    mPlat.getWindow().swapWindow();
-
-    // static constexpr uint64_t TIMESTEP_MS = 16;
-    // static uint64_t accum = 0;
-    // static uint64_t prevTime = Platform::getSysTimeMs();
-
-    // uint64_t currTime = Platform::getSysTimeMs();
-    // uint64_t frameTime = currTime - prevTime;
-    // if (frameTime > maxFrameTime) { frameTime = maxFrameTime; }
-    // accum += frameTime;
-
-    // while (accum >= TIMESTEP_MS)
-    // {
-    //     prevState = currState;
-    //     doStuff(currState);
-    //     accum -= TIMESTEP_MS;
-    // }
-
-    // double alpha = double(accum) / double(TIMESTEP_MS);
-    // lerpState = mix(prevState, currState, alpha);
-    // render(lerpState);
-}
+// }
 
 
-void idk::GfxService::shutdown(idk::IEngine*)
-{
-    VLOG_INFO("[GfxService::shutdown]");
-    mRen.shutdown();
-}
+// void idk::GfxService::update(idk::IEngine *E)
+// {
+//     mPlat.update();
+//     mRen.update(E);
+//     mPlat.getWindow().swapWindow();
+
+//     // static constexpr uint64_t TIMESTEP_MS = 16;
+//     // static uint64_t accum = 0;
+//     // static uint64_t prevTime = Platform::getSysTimeMs();
+
+//     // uint64_t currTime = Platform::getSysTimeMs();
+//     // uint64_t frameTime = currTime - prevTime;
+//     // if (frameTime > maxFrameTime) { frameTime = maxFrameTime; }
+//     // accum += frameTime;
+
+//     // while (accum >= TIMESTEP_MS)
+//     // {
+//     //     prevState = currState;
+//     //     doStuff(currState);
+//     //     accum -= TIMESTEP_MS;
+//     // }
+
+//     // double alpha = double(accum) / double(TIMESTEP_MS);
+//     // lerpState = mix(prevState, currState, alpha);
+//     // render(lerpState);
+// }
+
+
+// void idk::GfxService::shutdown(idk::IEngine*)
+// {
+//     VLOG_INFO("[GfxService::shutdown]");
+//     mRen.shutdown();
+// }
--- src/gfx2d/RenderEngine.cpp
+++ src/gfx2d/RenderEngine.cpp
@@ -1,63 +1,63 @@
-#include "idk/gfx2d/RenderEngine.hpp"
+// #include "idk/gfx2d/RenderEngine.hpp"
 
 
-idk::gfx::RenderEngine::RenderEngine(idk::Platform &plat)
-:   mPlat(plat),
-    mWin(plat.getWindow())
-{
-    int winWidth = mWin.getWidth();
-    int winHeight = mWin.getHeight();
-    mCamera = idk::Camera(float(winWidth)/winHeight, 80.0f, 0.1f, 8000.0f),
-    mCamera.getTransform().SetPosition(glm::vec3(0.0f, 16.0f, 32.0f));
-}
+// idk::gfx::RenderEngine::RenderEngine(idk::Platform &plat)
+// :   mPlat(plat),
+//     mWin(plat.getWindow())
+// {
+//     int winWidth = mWin.getWidth();
+//     int winHeight = mWin.getHeight();
+//     mCamera = idk::Camera(float(winWidth)/winHeight, 80.0f, 0.1f, 8000.0f),
+//     mCamera.getTransform().SetPosition(glm::vec3(0.0f, 16.0f, 32.0f));
+// }
 
 
-idk::gfx::RenderEngine::~RenderEngine()
-{
+// idk::gfx::RenderEngine::~RenderEngine()
+// {
 
-}
-
-
-void idk::gfx::RenderEngine::update(idk::IEngine *E)
-{
-    (void)E;
-
-}
+// }
 
 
-void idk::gfx::RenderEngine::shutdown()
-{
+// void idk::gfx::RenderEngine::update(idk::IEngine *E)
+// {
+//     (void)E;
 
-}
-
-
-void idk::gfx::RenderEngine::drawRect(const glm::vec2 &xy, const glm::vec2 &wh)
-{
-    (void)xy;
-    (void)wh;
-}
+// }
 
 
-void idk::gfx::RenderEngine::drawEllipse(const glm::vec2 &xy, const glm::vec2 &wh)
-{
-    (void)xy;
-    (void)wh;
-}
+// void idk::gfx::RenderEngine::shutdown()
+// {
+
+// }
 
 
-void idk::gfx::RenderEngine::setRefreshRateHz(uint64_t hz)
-{
-    (void)hz;
-}
+// void idk::gfx::RenderEngine::drawRect(const glm::vec2 &xy, const glm::vec2 &wh)
+// {
+//     (void)xy;
+//     (void)wh;
+// }
 
 
-idk::Camera &idk::gfx::RenderEngine::getCamera()
-{
-    return mCamera;
-}
+// void idk::gfx::RenderEngine::drawEllipse(const glm::vec2 &xy, const glm::vec2 &wh)
+// {
+//     (void)xy;
+//     (void)wh;
+// }
 
 
-void idk::gfx::RenderEngine::swapCamera()
-{
+// void idk::gfx::RenderEngine::setRefreshRateHz(uint64_t hz)
+// {
+//     (void)hz;
+// }
 
-}
+
+// idk::Camera &idk::gfx::RenderEngine::getCamera()
+// {
+//     return mCamera;
+// }
+
+
+// void idk::gfx::RenderEngine::swapCamera()
+// {
+
+// }
